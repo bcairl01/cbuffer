0508fe3e3a91e2c95f56974731f97a0b4d472124
Brian Cairl <briancairl@gmail.com>
2014-07-16 02:25:25 -0400

    First commit

---
+++ include/cbuffer.h
@@ -0,0 +1,28 @@
+#ifndef CBUFFER_H
+#define CBUFFER_H
+
+/// @brief 	Generic Ring-Buffer Structure
+/// @var 	capacity 		size of allocated buffer space
+/// @var	rd_idx			next slot in buffer to read from
+/// @var	wr_idx			next slot in buffer to write to
+/// @var	size			number of unread bytes in buffer
+/// @var	data			buffer stack space
+/// @var	overflow		buffer-overflow flag
+typedef struct 
+{
+	uint16_t				capacity;
+	uint16_t				rd_idx;
+	uint16_t				wr_idx;
+	uint16_t 				size;
+	uint8_t*				data;
+	uint8_t					overflow;
+} buffer;
+
+void 	buffer_init( buffer* buf_p );
+void 	buffer_clear( buffer* buf_p );
+void 	buffer_alloc( buffer* buf_p, size_t cap );
+void 	buffer_free( buffer* buf_p );
+uint8_t buffer_set( buffer* buf_p, uint8_t byte_in );
+uint8_t buffer_get( buffer* buf_p, uint8_t* byte_out );
+
+#endif
---
+++ source/cbuffer.c
@@ -0,0 +1,99 @@
+
+#include <cbuffer.h>
+
+
+/// Helper Macros
+#define BUFFER_SET_NEXT(buf_p,byte)		buf_p->data[buf_p->wr_idx++]=byte; ++buf_p->size
+#define BUFFER_GET_NEXT(buf_p,byte)		byte=buf_p->data[buf_p->rd_idx++]; --buf_p->size 
+#define BUFFER_GET_SIZE(buf_p)			(buf_p->size)
+#define BUFFER_WR_IDX_WRAP(buf_p)		buf_p->wr_idx%=buf_p->capacity
+#define BUFFER_RD_IDX_WRAP(buf_p)		buf_p->rd_idx%=buf_p->capacity
+#define BUFFER_OVERFLOW(buf_p)			buf_p->overflow=1UL;
+#define BUFFER_NO_OVERFLOW(buf_p)		buf_p->overflow=0UL;
+
+
+
+/// @brief 	Initializes support structure
+///	@param 	buf_p		pointer to buffer.
+void buffer_init( buffer* buf_p )
+{
+	buf_p->rd_idx 		= 
+	buf_p->wr_idx 		= 
+	buf_p->capacity 	= 0UL;
+	buf_p->size 		= 0UL;
+	buf_p->overflow 	= 0UL;
+	buf_p->data 		= NULL;
+}
+
+
+
+/// @brief 	Zeros available buffer space
+///	@param 	buf_p		pointer to buffer.
+void buffer_clear( buffer* buf_p )
+{
+	if(buf_p->data)
+	{
+		size_t idx = 0;
+		for( ; idx < buf_p->capacity; idx++ )
+			buf_p->data[idx]=0;
+	}
+}
+
+
+
+/// @brief 	Allocates buffer space
+///	@param 	buf_p		pointer to buffer.
+/// @param 	cap 		buffer capacity
+void buffer_alloc( buffer* buf_p, size_t cap )
+{
+	buf_p->capacity = cap;
+	buf_p->data 	= (uint8_t*)malloc(cap*sizeof(uint8_t));
+
+	buffer_clear(buf_p);
+}
+
+
+
+/// @brief 	Frees allocated buffer space
+///	@param 	buf_p		pointer to buffer.
+void buffer_free( buffer* buf_p )
+{
+	buf_p->capacity = 0;
+	free(buf_p->data);
+	buf_p->data = NULL;
+}
+
+
+
+/// @brief 	Adds single byte to the  buffer
+///	@param 	buf_p		pointer to buffer.
+/// @param 	byte_in 	byte to add
+/// @return '1' upon success
+uint8_t buffer_set( buffer* buf_p, uint8_t byte_in )
+{
+	if( BUFFER_GET_SIZE(buf_p) < buf_p->capacity )
+	{
+		BUFFER_SET_NEXT(buf_p,byte_in);
+		BUFFER_WR_IDX_WRAP(buf_p);
+		return 1UL;
+	}
+	BUFFER_OVERFLOW(buf_p);
+	return 0UL;
+}
+
+
+
+/// @brief 	Gets single byte from the buffer
+///	@param 	buf_p		pointer to buffer.
+/// @param 	byte_out 	output byte
+/// @return '1' upon success
+uint8_t buffer_get( buffer* buf_p, uint8_t* byte_out )
+{	
+	if( BUFFER_GET_SIZE(buf_p) )
+	{
+		BUFFER_GET_NEXT(buf_p,*byte_out);
+		BUFFER_RD_IDX_WRAP(buf_p);
+		return 1UL;
+	}
+	return 0UL;
+}
